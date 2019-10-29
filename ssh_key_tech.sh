a02e11b159f74a1fc77e2de54dfce8719b84e590
birju kumar <32246823+birjuk7@users.noreply.github.com>
2019-10-29 22:18:49 +0530

    passwordless ssh

--- ssh_key_tech.sh
+++ ssh_key_tech.sh
@@ -1,6 +1,7 @@
 #step 1
 cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
-#step 2
+#step 2 
+#     file in local comp   remote we want to ssh
 Scp ~/.ssh/authorized_keys root@192.168.xx.xx:~/.ssh
 #step 3
 chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys
